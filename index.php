74a0265530b715b1fd991a19e1eda92cfed3b371
EunaceEmactao <kimeunna.917@gmail.com>
2026-09-20 17:27:21 +0800

    added student display table on index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Database Connection
+//Database Connection
 $host = 'localhost';
 $db = 'library_db';
 $user = 'root';
@@ -66,6 +66,47 @@ if($section==='students'){
     <hr>
     <?php if($section === 'students'): ?>
         <h1>Students</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Course</th>
+                    <th>Created at</th>
+                    <th>Actions</th>
+                </tr>
+            <thead>
+            <tbody>
+                <?php foreach($students as $student): ?>
+                    <tr>
+                        <td>
+                            <?=htmlspecialchars($student['student_id']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_first_name']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_last_name']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_course']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_created_at']) ?>
+                        </td>
+                         <td>
+                            <a>Edit</a>
+                            |
+                            <a>Delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach?>
+            </tbody>
+
+        </table>
+
+
 
     <?php endif;?>
 
